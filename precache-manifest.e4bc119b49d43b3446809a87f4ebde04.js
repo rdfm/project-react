cb7da83358e84abd1d916551490286e79c9099a3
Raul Francisco-Menchavez <raulmenchavez@gmail.com>
2019-03-01 14:38:30 -0800

    Updates

---
+++ precache-manifest.e4bc119b49d43b3446809a87f4ebde04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "60dc893bfb5ad56be4ae",
+    "url": "/project-react/static/js/runtime~main.60dc893b.js"
+  },
+  {
+    "revision": "3f03a68b9e7ec0ba72c3",
+    "url": "/project-react/static/js/main.3f03a68b.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/project-react/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "3f03a68b9e7ec0ba72c3",
+    "url": "/project-react/static/css/main.b75e47d8.chunk.css"
+  },
+  {
+    "revision": "78a6092a6e4e34ca995111bc1a559283",
+    "url": "/project-react/index.html"
+  }
+];
