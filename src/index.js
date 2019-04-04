745d5b910e0a76e42db49c8b23278c7b8ba6a7fb
Raul Francisco-Menchavez <raulmenchavez@gmail.com>
2019-04-04 16:03:50 -0700

    Taking Turns.

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
@@ -38,8 +39,11 @@ class Board extends React.Component {
         // Immutability Is Important
         // .slice() operator to create a copy of the squares array
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -53,7 +57,7 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
