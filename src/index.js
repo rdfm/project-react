ede9de67080162119bbb7e0caff08e0427767926
Raul Francisco-Menchavez <raulmenchavez@gmail.com>
2019-04-04 16:35:14 -0700

    In index.js, Declaring a Winner, by adding helper function and conditionals. In index.css, updated CSS and added title.

--- src/index.js
+++ src/index.js
@@ -39,6 +39,9 @@ class Board extends React.Component {
         // Immutability Is Important
         // .slice() operator to create a copy of the squares array
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -57,10 +60,18 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  
+        // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner + ' !!!';
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
       return (
         <div>
+          <div className="title">React: Tic Tac Toe</div>
           <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
@@ -104,3 +115,26 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+// ========================================
+// Helper Function
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
